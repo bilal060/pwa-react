b6daa4ebdd20e3a148c1f7fba5cc2249d7ea9942
Bilal060 <ahmad_4al@hotmail.com>
2023-07-03 16:53:49 +0500

    latest code requied

---
+++ src/Components/FilterForm/DispensaryFrom.js
@@ -0,0 +1,134 @@
+import React from "react";
+
+const DispensaryFrom = () => {
+  return (
+    <>
+      <form onSubmit={(e) => submitHandler(e)}>
+        <div className="d-flex flex-md-row flex-column align-items-center gap-4 justify-content-between mb-4">
+          <div className="form-control h-auto p-0 bg-transparent border-0">
+            <label className="text-white mb-2 font-weight-600 font-18-100">
+              Post Strain
+            </label>
+            <select
+              className="auth-input"
+              required
+              name="postStrain"
+              value={dispensary.postStrain}
+              onChange={(e) => formHandler(e)}
+            >
+              <option value={""}>- Select Strain -</option>
+              <option value="Sativa">Sativa</option>
+              <option value="Indica">Indica</option>
+              <option value="Hybrid">Hybrid</option>
+              <option value="CBD">CBD</option>
+            </select>
+          </div>
+          <div className="form-control h-auto p-0 bg-transparent border-0">
+            <label className="text-white mb-2 font-weight-600 font-18-100">
+              Quantity
+            </label>
+            <select
+              className="auth-input"
+              required
+              value={dispensary.quantity}
+              name="quantity"
+              onChange={(e) => formHandler(e)}
+            >
+              <option value={""}>- Select Quantity -</option>
+              <option value={"1-7"}>3-5 Grams</option>
+              <option value={"4-14"}>5-10 Grams</option>
+              <option value={"14-30"}>10-15 Grams</option>
+            </select>
+          </div>
+        </div>
+        <div className="d-flex flex-md-row flex-column align-items-center gap-4 justify-content-between mb-4">
+          <div className="form-control h-auto p-0 bg-transparent border-0">
+            <label className="text-white mb-2 font-weight-600 font-18-100">
+              Cost
+            </label>
+            <input
+              onChange={(e) => formHandler(e)}
+              type="number"
+              className="auth-input"
+              placeholder="$ Enter Cost"
+              required
+              value={dispensary.cost}
+              name="cost"
+            />
+          </div>
+          <div className="form-control h-auto p-0 bg-transparent border-0">
+            <label className="text-white mb-2 font-weight-600 font-18-100">
+              Strain Name
+            </label>
+            <input
+              onChange={(e) => formHandler(e)}
+              type="text"
+              className="auth-input"
+              placeholder="Enter Strain Name"
+              required
+              value={dispensary.strainName}
+              name="strainName"
+            />
+          </div>
+        </div>
+        <div className="form-control h-auto p-0 bg-transparent border-0 mb-4">
+          <label className="text-white mb-2 font-weight-600 font-18-100">
+            Description
+          </label>
+          <textarea
+            onChange={(e) => formHandler(e)}
+            className="auth-input-textarea"
+            placeholder="Enter description here..."
+            required
+            name="description"
+            value={dispensary.description}
+          />
+        </div>
+
+        <label className="text-white mb-2 font-weight-600 font-18-100">
+          Upload Images
+        </label>
+        <div className="d-flex flex-md-row flex-column align-items-center gap-4 justify-content-between mb-4">
+          <label className="upload-file cr-p">
+            <input
+              type="file"
+              className="d-none"
+              accept=".jpg, .jpeg, .png"
+              multiple
+              onChange={(e) => attachFile(e)}
+            />
+
+            <div className="d-flex justify-content-center align-items-center h-100 w-100 gap-2">
+              <UploadIcon />
+              <p className="font-16 font-weight-500">
+                {file === null ? "Choose File / Drag & Drop Here" : file}
+              </p>
+            </div>
+          </label>
+          <button
+            className="add-more bg-transparent border-white text-white gap-2"
+            onClick={() => addMore()}
+            type="button"
+          >
+            <AddIcon />
+            Add More Strain {savedDataCount}
+          </button>
+        </div>
+        <div className="d-flex flex-sm-row flex-column align-items-center gap-4 justify-content-center  mt-5">
+          <button
+            className="green-btn-outline custom-w min-width-208"
+            onClick={() => goBack()}
+            type="button"
+          >
+            Back
+          </button>
+          <button className="green-btn custom-w min-width-208" type="submit">
+            Next
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default DispensaryFrom;
---
+++ src/Components/FilterForm/SeedstoreForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SeedstoreForm = () => {
+  return <></>;
+};
+
+export default SeedstoreForm;
