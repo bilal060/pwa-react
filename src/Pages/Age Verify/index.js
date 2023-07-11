3b9a9ea812f8473207eea8709a444cc125adc9a6
Bilal060 <ahmad_4al@hotmail.com>
2023-07-11 23:14:00 +0500

    date and sates issue set

--- src/Pages/Age Verify/index.js
+++ src/Pages/Age Verify/index.js
@@ -38,6 +38,9 @@ const AgeVerifyPage = () => {
     e.preventDefault();
     VerifyAge(ageVerify, navigate);
   };
+  const handleDatePickerChange = (date) => {
+    onChange(date);
+  };
   return (
     <div className="max-width-521">
       <h2 className="auth-model-heading mb-4">Confirm Your Age</h2>
@@ -56,21 +59,39 @@ const AgeVerifyPage = () => {
         >
           <option value="">Where are you from?</option>
           <option value="AB">Alberta(AB)</option>
+          <option value="AL">Alaska</option>
+          <option value="AR">Arizona</option>
           <option value="BC">British Columbia(BC)</option>
+          <option value="CA">California</option>
+          <option value="CL">Colorado</option>
+          <option value="CO">Connecticut</option>
+          <option value="IL">Illinois</option>
+          <option value="MA">Maine</option>
+          <option value="MS">Massachusetts</option>
+          <option value="MI">Michigan</option>
+          <option value="MO">Montana</option>
           <option value="MB">Manitoba(MB)</option>
+          <option value="NE">Nevada</option>
+          <option value="NJ">New Jersey</option>
+          <option value="NM">New Mexico</option>
+          <option value="NY">New York</option>
           <option value="NB">New Brunswick(NB)</option>
           <option value="NL">Newfoundland and Labrador(NL)</option>
           <option value="NT">Northwest Territories(NT)</option>
           <option value="NS">Nova Scotia(NS)</option>
           <option value="NU">Nunavut(NU)</option>
           <option value="ON">Ontario(ON)</option>
+          <option value="OR">Oregon</option>
           <option value="PE">Prince Edward Island (PE)</option>
           <option value="Quebec">Quebec(QC)</option>
+          <option value="VE">Vermont</option>
+          <option value="VI">Virginia</option>
+          <option value="WA">Washington</option>
           <option value="SK">Saskatchewan(SK)</option>
           <option value="YT">Yukon(YT)</option>
         </select>
         <DatePicker
-          onChange={onChange}
+          onChange={handleDatePickerChange}
           value={value}
           className="auth-input"
           clearIcon={false}
@@ -80,6 +101,7 @@ const AgeVerifyPage = () => {
           monthPlaceholder="MM"
           calendarIcon={<CalendarIcon />}
           required={true}
+          maxDate={new Date()}
         />
         <div className="d-flex flex-sm-row flex-column align-items-center gap-4 justify-content-between mt-5">
           <button className="green-btn-outline" onClick={() => navigate(-1)}>
